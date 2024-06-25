014d36a27eb0eb77bb751dd63d002feab9e17210
Caleb Perry <caleb@foxtrotperry.com>
2024-06-25 02:47:08 -0400

    hooking up nws api

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "~/components/ui/button";
 import { api } from "~/trpc/server";
 
 export default async function Home() {
-  const forecastQuery = await api.forecast.get();
+  const forecastQuery = await api.forecast.get({ wfo: "BGM", x: 52, y: 104 });
 
   console.log(forecastQuery.data);
 
--- webapp/src/server/api/routers/forecast.ts
+++ webapp/src/server/api/routers/forecast.ts
@@ -1,9 +1,21 @@
 import axios from "axios";
+import z from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { zWfoEnum } from "~/types/nws";
 
 export const forecastRouter = createTRPCRouter({
-  get: publicProcedure.query(() => {
-    return axios.get("https://api.weather.gov/gridpoints/BGM/52,103/forecast");
-  }),
+  get: publicProcedure
+    .input(
+      z.object({
+        wfo: zWfoEnum,
+        x: z.number(),
+        y: z.number(),
+      }),
+    )
+    .query(async ({ input }) => {
+      return axios.get(
+        `https://api.weather.gov/gridpoints/${input.wfo}/${input.x},${input.y}`,
+      );
+    }),
 });
