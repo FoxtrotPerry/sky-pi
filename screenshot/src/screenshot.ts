508405e5c0f60fe477a66b1df3ee29470b36d618
Caleb Perry <caleb@foxtrotperry.com>
2024-07-11 02:48:05 -0400

    adding webapp screenshot script

---
+++ screenshot/src/screenshot.ts
@@ -0,0 +1,22 @@
+import puppeteer from "puppeteer";
+
+// Resolution of typical e-ink display
+const [width, height] = [800, 480];
+
+const browser = await puppeteer.launch({
+  headless: true,
+  args: [`--window-size=${width},${height}`],
+  defaultViewport: {
+    width: width,
+    height: height,
+  },
+});
+const page = await browser.newPage();
+await page.goto("http://localhost:3000", {
+  waitUntil: "networkidle2",
+});
+await page.screenshot({
+  path: "weather.png",
+});
+
+await browser.close();
