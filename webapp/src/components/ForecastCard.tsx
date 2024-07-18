dfdee27fa4a70a2adcdb40e6caa04f3eaef634c2
Caleb Perry <caleb@foxtrotperry.com>
2024-07-18 03:20:58 -0400

    good time frames are more apparent

--- webapp/src/components/ForecastCard.tsx
+++ webapp/src/components/ForecastCard.tsx
@@ -1,10 +1,11 @@
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import type { NWSDataPoint } from "~/types/forecast";
 import { format, getHours } from "date-fns";
-import { MoonStar } from "lucide-react";
+import { MoonStar, CloudMoon } from "lucide-react";
 import { percentToSkyShade } from "~/lib/utils/tailwind";
 import { useCallback } from "react";
 import { Cloudy } from "~/components/icons/cloudy";
+import clsx from "clsx";
 
 type ForecastCardProps = {
   skyCoverData: NWSDataPoint[];
@@ -15,6 +16,7 @@ export const ForecastCard = ({ skyCoverData }: ForecastCardProps) => {
 
   const getIcon = useCallback((value: number) => {
     if (20 >= value) return MoonStar;
+    if (40 >= value) return CloudMoon;
     return Cloudy;
   }, []);
 
@@ -33,21 +35,45 @@ export const ForecastCard = ({ skyCoverData }: ForecastCardProps) => {
       <CardContent className="space-y-0.5 px-3 pb-3 pt-0.5">
         <div className="flex flex-row justify-end gap-1">
           {skyCoverData.map((forecast) => {
-            const shade = percentToSkyShade(forecast?.value ?? 50);
+            if (!forecast?.value) return;
+            const value = forecast?.value;
+            const highlight = 20 >= value;
+            const shade = percentToSkyShade(value);
             const hour = forecast?.validTime
               ? getHours(forecast?.validTime.date) + 1
               : 0;
             // only show the hour if it's cleanly divisible by 6 or is 1
             const showHour = hour % 6 === 0 || hour === 1;
-            const Icon = getIcon(forecast?.value ?? 100);
+            const Icon = getIcon(value);
             return (
               <div
-                className="flex flex-col items-center justify-end"
+                className={clsx(
+                  "flex flex-col items-center justify-end",
+                  highlight &&
+                    showHour &&
+                    "gap-0 rounded-lg border-2 bg-slate-100",
+                )}
                 key={`sky-cover-${forecast?.validTime.date.valueOf()}`}
               >
                 {showHour && <p className="text-slate-500">{hour}</p>}
-                <Icon className={`fill-slate-${shade} stroke-slate-500`} />
-                <p className="text-xs text-slate-500">{forecast?.value}</p>
+                <div
+                  className={clsx(
+                    "flex flex-col items-center",
+                    highlight &&
+                      !showHour &&
+                      "gap-0 rounded-lg border-2 bg-slate-100",
+                  )}
+                >
+                  <Icon className={`fill-slate-${shade} stroke-slate-500`} />
+                  <p
+                    className={clsx(
+                      "text-xs text-slate-500",
+                      highlight && "font-bold text-slate-800",
+                    )}
+                  >
+                    {value}
+                  </p>
+                </div>
               </div>
             );
           })}
