9f7c5ff63b4e977c3367049e0debbf1f1cf3b777
Caleb Perry <caleb@foxtrotperry.com>
2024-07-29 02:52:34 -0400

    squashing start script bugs

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,8 +2,9 @@
 
 # if node is installed...
 if command -v node &> /dev/null # "&> /den/null" mutes output
+then
   # ...then start up the web app
-  node ./packages/webapp/server.js
+  node ./webapp/packages/webapp/server.js
 else
   # ...otherwise, prompt to run setup script and exit with error
   echo "Node.js is not installed. Please run ./setup.sh to proceed."
