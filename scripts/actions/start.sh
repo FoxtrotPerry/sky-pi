e119d0e986d0e224b0c683b2d596f18b871c0f58
Caleb Perry <caleb@foxtrotperry.com>
2024-08-06 00:38:47 -0400

    fixing paths to executable scripts

--- scripts/actions/start.sh
+++ scripts/actions/start.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+SKYPI_DIR="$HOME/.sky-pi/"
+
 # if node is installed...
 if command -v node &> /dev/null # "&> /den/null" mutes output
 then
   # ...then start up the web app
-  node ../webapp/packages/webapp/server.js
+  node $SKYPI_DIR/sky-pi/webapp/packages/webapp/server.js
 else
   # ...otherwise, prompt to run setup script and exit with error
   echo "Node.js is not installed. Please run ./setup.sh to proceed."
