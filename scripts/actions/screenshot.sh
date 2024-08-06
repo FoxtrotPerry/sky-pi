18ccfe6b429144d59196945d9e67a400f4ba6073
FoxtrotPerry <31854658+FoxtrotPerry@users.noreply.github.com>
2024-08-06 00:39:48 -0400

    fixing paths to executable scripts (#10)

--- scripts/actions/screenshot.sh
+++ scripts/actions/screenshot.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+SKYPI_DIR="$HOME/.sky-pi/"
+
 # if node is installed...
 if command -v node &> /dev/null
 then
   # ...then run the screenshot service
-  node ../screenshot/screenshot.js
+  node $SKYPI_DIR/sky-pi/screenshot/screenshot.js
 else
 # ...otherwise, prompt to run setup script and exit with error
   echo "Node.js is not installed. Please run ./setup.sh to proceed."
