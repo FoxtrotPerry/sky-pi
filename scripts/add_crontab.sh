0075397b31353a5fd0c8e114223cf4609417a4a6
Caleb Perry <caleb@foxtrotperry.com>
2024-08-10 00:59:32 -0400

    adding start of crontab rm file

--- scripts/add_crontab.sh
+++ scripts/add_crontab.sh
@@ -2,10 +2,12 @@
 
 SKYPI_DIR="$HOME/.sky-pi"
 
-#write out current crontab
-crontab -l > mycron
-#echo new cron into cron file
-echo "*/15 * * * * node $SKYPI_DIR/sky-pi/screenshot/screenshot.js" >> mycron
-#install new cron file
-crontab mycron
-rm mycron
+# write out current crontab config
+crontab -l > tmp_crontab
+# echo screenshot crontab entry into temp file
+echo "*/15 * * * * node $SKYPI_DIR/sky-pi/screenshot/screenshot.js" >> tmp_crontab
+# echo eink_draw crontab entry into temp file
+# TODO: ADD EINK DRAW CRONTAB ENTRY HERE
+# install new cron file
+crontab tmp_crontab
+rm tmp_crontab
--- scripts/rm_crontab.sh
+++ scripts/rm_crontab.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-
+# write out current crontab config
+crontab -l > curr_crontab
+# create clone of crontab config "new_crontab" that has all lines
+# mentioning "sky-pi" removed
+grep -v "sky-pi" curr_crontab > new_crontab
+# install new cron file
+crontab new_crontab
+# delete tmp files
+rm curr_crontab
+rm old_crontab
