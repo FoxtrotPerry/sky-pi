9559c4690aa7977a9e67d16b5a850ad7254f8488
Caleb Perry <caleb@foxtrotperry.com>
2024-08-19 23:03:00 -0400

    adding draw execute to crontab setup

--- scripts/add_crontab.sh
+++ scripts/add_crontab.sh
@@ -4,10 +4,9 @@ SKYPI_DIR="$HOME/.sky-pi"
 
 # write out current crontab config
 crontab -l > tmp_crontab
-# echo screenshot crontab entry into temp file
-echo "*/15 * * * * node $SKYPI_DIR/sky-pi/screenshot/screenshot.js" >> tmp_crontab
-# echo eink_draw crontab entry into temp file
-# TODO: ADD EINK DRAW CRONTAB ENTRY HERE
+# echo screenshot and draw crontab entry into temp file
+# Why we need to use bash specifically: https://askubuntu.com/a/752245
+echo "*/15 * * * * node $SKYPI_DIR/sky-pi/screenshot/screenshot.js && bash -c '$SKYPI_DIR/sky-pi/actions/draw.sh'" >> tmp_crontab
 # install new cron file
 crontab tmp_crontab
 rm tmp_crontab
