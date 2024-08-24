602d4f8fafba4148ce4d6fac50da0a59ce9cd648
Caleb Perry <caleb@foxtrotperry.com>
2024-08-24 00:31:09 -0400

    adding refresh script

--- scripts/add_crontab.sh
+++ scripts/add_crontab.sh
@@ -7,6 +7,8 @@ crontab -l > tmp_crontab
 # echo screenshot and draw entry into temp file
 # Why we need to use bash specifically: https://askubuntu.com/a/752245
 echo "*/15 * * * * node $SKYPI_DIR/sky-pi/screenshot/screenshot.js && bash -c '$SKYPI_DIR/sky-pi/actions/draw.sh'" >> tmp_crontab
+# Refresh the display every 12 hours (at midnight and noon)
+echo "0 */12 * * * bash -c '$SKYPI_DIR/sky-pi/actions/refresh.sh'" >> tmp_crontab
 # echo app startup entry into temp file
 echo "@reboot bash -c '$SKYPI_DIR/sky-pi/actions/start.sh'" >> tmp_crontab
 # install new cron file
---
+++ sky-pi-draw/src/sky_pi_draw/refresh.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import time
+
+from inky.auto import auto
+from inky.inky_uc8159 import CLEAN
+
+inky = auto(ask_user=True, verbose=True)
+
+colors = ["Black", "White", "Green", "Blue", "Red", "Yellow", "Orange"]
+
+# Cycle through all available colors
+for color in range(7):
+    print("Color: {}".format(colors[color]))
+    for y in range(inky.height):
+        for x in range(inky.width):
+            inky.set_pixel(x, y, color)
+    inky.set_border(color)
+    inky.show()
+    time.sleep(5.0)
+
+# Clear the display
+for _ in range(2):
+    for y in range(inky.height - 1):
+        for x in range(inky.width - 1):
+            inky.set_pixel(x, y, CLEAN)
+
+        inky.show()
+        time.sleep(1.0)
