8ff4923fd3dad85aa80fac40a78c12c49ef21238
Caleb Perry <caleb@foxtrotperry.com>
2024-09-08 03:09:44 -0400

    fixing os detection in install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -23,6 +23,11 @@ info_echo() {
   echo "⛅️ ${bold}[INFO]${normal}: $message"
 }
 
+warn_echo() {
+  local message="$1"
+  echo "⚠️ ${bold}${yellow}[WARN]${normal}${yellow}: $message${normal}"
+}
+
 error_echo() {
   local message="$1"
   echo "❌ ${bold}${red}[ERROR]${normal}${red}: $message${normal}" >&2
@@ -38,7 +43,7 @@ clear
 echo "🌓 ${bold}${magenta}SKY PI Installation${normal} 🌗"
 printf "\n"
 
-if [-f "$RPI_SOURCE_LIST" && grep -q "http://archive.raspberrypi.com/debian/" $RPI_SOURCE_LIST]; then
+if [ -f "$RPI_SOURCE_LIST" && grep -q "http://archive.raspberrypi.com/debian/" $RPI_SOURCE_LIST ]; then
   info_echo "Raspbian OS detected"
 else
   warn_echo "Rasbian OS not detected! SKY PI might not work properly. If you run into problems, please create an issue on GitHub!"
