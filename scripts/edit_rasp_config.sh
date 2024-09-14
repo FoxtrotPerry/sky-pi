bcdc84b9005fb9a809bd829f2670a9c24324843f
Caleb Perry <caleb@foxtrotperry.com>
2024-09-13 23:16:54 -0400

    prompting for password so tee can operate on config.txt

--- scripts/edit_rasp_config.sh
+++ scripts/edit_rasp_config.sh
@@ -3,6 +3,24 @@
 # This is necessary for the pi to talk to the eink HAT.
 # More info: https://github.com/pimoroni/inky/issues/202
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+red=$(tput setaf 1)
+magenta=$(tput setaf 5)
+cyan=$(tput setaf 6)
+
+info_echo() {
+  local message="$1"
+  echo "⛅️ ${bold}[INFO]${normal}: $message"
+}
+
+ask_echo() {
+  local message="$1"
+  echo "❔ ${cyan}[CONFIG]: $message${normal}"
+}
+
 # If not using Bookworm, config is located at /boot/config.txt
 CONFIG_FILE="/boot/firmware/config.txt"
 
@@ -11,6 +29,10 @@ LINE="dtoverlay=spi0-0cs"
 
 # Add SPI option to the raspberry pi's config file if it doesn't already exist
 if ! grep -q "^${LINE}$" "$CONFIG_FILE"; then
-    echo "$LINE" | tee -a "$CONFIG_FILE" > /dev/null
-    echo "Line added: $LINE"
+  info_echo "Required config setting not found. Adding..."
+  ask_echo "You might be prompted for your password to edit the config file."
+  echo "$LINE" | tee -a "$CONFIG_FILE" > /dev/null
+  info_echo "Raspberry Pi config updated"
+else
+  info_echo "Required config setting already exists in $CONFIG_FILE"
 fi
